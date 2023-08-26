488871b356430158b64f3da7724ec72274a8e931
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-08-26 17:40:38 -0300

    finaliza script

--- cria_txt.js
+++ cria_txt.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 
 function criaTxt(array, nomeArquivo) {
-    
+    const cadaLinhaMap = array.map(linha => linha.join(','))
+    fs.writeFileSync(nomeArquivo, cadaLinhaMap.join('\n'))
 }
 
-const
+let numberOne = 1;
+let numberTwo = 10;
 
-criaTxt()
+const array = [];
+for (let index = `${numberOne}`; index <= `${numberTwo}`; index++) {
+    array.push([index, 7777, 'DDR']);
+}
+
+criaTxt(array, 'arquivo.txt')
