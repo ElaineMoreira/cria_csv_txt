1ce8a1e4c4fc7f3cbcf5bf62a39253d6b337d8ff
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-08-27 09:46:27 -0300

    adiciona modificação

--- cria_txt.js
+++ cria_txt.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
+const readline = require('readline-sync');
+
+let numberOne = 10;
+let numberTwo = 30;
+let cnl = 7779;
+let tipoDeLinha = 'DDR';
 
 function criaTxt(array, nomeArquivo) {
     const cadaLinhaMap = array.map(linha => linha.join(','))
     fs.writeFileSync(nomeArquivo, cadaLinhaMap.join('\n'))
 }
-
-let numberOne = 10;
-let numberTwo = 100;
-let cnl = 7779;
-let tipoDeLinha = 'DDR';
 
 const array = [];
 for (let index = `${numberOne}`; index <= `${numberTwo}`; index++) {
